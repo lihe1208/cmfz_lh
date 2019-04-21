7cd5068cf3785faef2af453b60c4210697f412d7
lihe <1815939234@qq.com>
2019-04-21 16:08:42 +0800

    v1.3

--- src/main/java/com/baizhi/eitity/UserCount.java
+++ src/main/java/com/baizhi/eitity/UserCount.java
@@ -1,8 +1,16 @@
 package com.baizhi.eitity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCount {
+
     private String name;
     private Integer value;
 }
